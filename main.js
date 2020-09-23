7d55a386ac05bc0922265251a65207a90c58da23
Nihad <nihadkalathingal@gmail.com>
2020-09-22 22:54:42 -0400

    add eight puzzle and solving A*

--- main.js
+++ main.js
@@ -68,29 +68,98 @@ class EightPuzzleNode {
     this.level = level;
     // the evaluation score  f(n) = g(n) + h(n) where h is the heuristic
     this.evalScore = evalScore;
+    // js method binding
     this.getChildNodes = this.getChildNodes.bind(this);
   }
 
   getChildNodes() {
-    [blankX, blankY] = arrayFind(this.state, BLANK);
+    // find the blank index
+    const [blankX, blankY] = arrayFind(this.state, BLANK);
+    // try all possible moves
     const moves = [
       [blankX - 1, blankY],
       [blankX + 1, blankY],
       [blankX, blankY - 1],
       [blankX, blankY + 1],
     ];
+    // create a node for each valid move
     const validChildren = [];
     moves.forEach(([moveX, moveY]) => {
       const childState = moveBlank(this.state, blankX, blankY, moveX, moveY);
+      // only add if move is valid and creates a valid board state
       if (childState)
-        validChildren.push(EightPuzzleNode(childState, this.level + 1, null));
+        validChildren.push(
+          new EightPuzzleNode(childState, this.level + 1, null)
+        );
     });
 
     return validChildren;
   }
 }
 
-[blankX, blankY] = arrayFind(GOAL_STATE, BLANK);
-console.log(moveBlank(GOAL_STATE, blankX, blankY, 1, 2));
+// first heuristic: number of misplaced time i.e. h(x)
+function misplacedHeuristic(currentState, goalState) {
+  score = 0;
+  for (let i = 0; i < goalState.length; i++)
+    for (let j = 0; j < goalState[0].length; j++) {
+      if (currentState[i][j] !== goalState[i][j]) score += 1;
+    }
+  return score;
+}
+
+// evaluation function for the A* algorithm i.e. f(x)
+function evalFunction(currentState, goalState, level) {
+  return misplacedHeuristic(currentState, goalState) + level;
+}
+
+// the driver for the eight puzzle
+class EightPuzzle {
+  constructor(initialState) {
+    this.start = new EightPuzzleNode(initialState, 0, null);
+    this.open = [];
+    this.closed = [];
+  }
+
+  solve() {
+    // compute evaluation function for start node
+    this.start.evalScore = evalFunction(
+      this.start.state,
+      GOAL_STATE,
+      this.start.level
+    );
+    // add start node to open list
+    this.open.push(this.start);
+
+    while (true) {
+      // pop the best node
+      let currentNode = this.open.shift();
+
+      console.log(currentNode.level, currentNode.state);
+      // solution reached if heuristic is 0
+      if (misplacedHeuristic(currentNode.state, GOAL_STATE) === 0) break;
+
+      // compute eval function for each child node
+      currentNode.getChildNodes().forEach((childNode) => {
+        childNode.evalScore = evalFunction(
+          childNode.state,
+          GOAL_STATE,
+          childNode.level
+        );
+        this.open.push(childNode);
+      });
+      // sort the open list by the evaluation scores
+      this.open.sort((nodeA, nodeB) => nodeA.evalScore - nodeB.evalScore);
+      // close the current node
+      this.closed.push(currentNode);
+    }
+  }
+}
+
+puzzle = new EightPuzzle([
+  [2, 8, 3],
+  [1, 6, 4],
+  [7, BLANK, 5],
+]);
+puzzle.solve();
 
 renderState(GOAL_STATE);
